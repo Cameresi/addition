7e34a591509e81253bf831269c78b6dde9e38c60
Aaron Cameresi <aaroncameresi@gmail.com>
2017-06-27 09:16:06 -0400

    Update 1

---
+++ addition.rb
@@ -0,0 +1,16 @@
+
+
+#Aaron Cameresi			Addition Calculator
+
+#addition
+
+def addition
+		print "enter an integer.\n"
+			a = gets.to_i
+		print "enter an integer to add.\n"
+			b = gets.to_i
+			c = (a + b)
+		print "#{a} plus #{b} equals #{c} .\n"
+end
+
+addition
