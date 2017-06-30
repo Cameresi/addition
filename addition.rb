b10cba998c6125550967fd371578e0589e65eba4
Aaron Cameresi <aaroncameresi@gmail.com>
2017-06-30 16:14:51 -0400

    update to pass numbers

--- addition.rb
+++ addition.rb
@@ -4,13 +4,23 @@
 
 #addition
 
-def addition
-		print "enter an integer.\n"
-			a = gets.to_i
-		print "enter an integer to add.\n"
-			b = gets.to_i
-			c = (a + b)
-		print "#{a} plus #{b} equals #{c} .\n"
+# def addition
+# 		print "enter an integer.\n"
+# 			a = gets.to_i
+# 		print "enter an integer to add.\n"
+# 			b = gets.to_i
+# 			c = (a + b)
+# 		print "#{a} plus #{b} equals #{c} .\n"
+# end
+
+# addition
+
+
+def addition(number1, number2)
+
+	total = number1 + number2
+	p total
+
 end
 
-addition
+addition(3, 4)
